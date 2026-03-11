ebe91fff29a40d90688b92127ebb403f44595e12
kunfirekun <kunfirekun@gmail.com>
2026-03-11 10:31:12 +0300

    token removal, added manual override to counter connection loss, added comments for easier code readability

--- main.py
+++ main.py
@@ -5,21 +5,22 @@ import network
 import BlynkLib
 
 # --- 1. BLYNK CONFIGURATION ---
-BLYNK_TEMPLATE_ID = "TMPL23lcFNtXy"
+BLYNK_TEMPLATE_ID = ""
 BLYNK_TEMPLATE_NAME = "GROUP 5 FAN AUTOMATION"
 BLYNK_AUTH_TOKEN = ""
 
+
 # --- 2.ESP32 AND DHT22 HARDWARE CONFIGURATION ---
-sensor = dht.DHT22(machine.Pin(15))
-fan = machine.Pin(2, machine.Pin.OUT) # Built-in LED acts as our Fan
+sensor = dht.DHT22(machine.Pin(15)) # Acts as out temperature sensor
+fan = machine.Pin(2, machine.Pin.OUT) # LED acts as our fan
 
 # --- 3. GLOBAL VARIABLES ---
-TEMP_ON = 30.0   # Upper Threshold
-TEMP_OFF = 28.0  # Lower Threshold
+TEMP_ON = 30.0   # Upper temperature threshold
+TEMP_OFF = 28.0  # Lower temperature threshold
 last_update = 0
-update_interval = 5000 # 5 seconds to prevent -104 errors
-manual_fan = 0   # State of the Blynk Switch (V3)
-auto_fan = 0     # State calculated by the sensor
+update_interval = 3500 # 3.5 seconds delay remittance of data to prevent stale data
+manual_fan = 0   # State of the Manual overide switch
+auto_fan = 0     # Fan original state
 
 # --- 4. WIFI CONNECTION ---
 print("Connecting to Wokwi-GUEST...")
@@ -47,7 +48,7 @@ def v3_handler(value):
     manual_fan = int(value[0])
     print(f"Manual Overide Switch is: {'ON' if manual_fan else 'OFF'}")
 
-# --- 6. MAIN LOOP ---
+# --- 6. The program to run the fan starts here with a while loop ---
 print("Group 5 Fan Starting...")
 
 while True:
@@ -56,29 +57,29 @@ while True:
         
         current_time = time.ticks_ms()
         if time.ticks_diff(current_time, last_update) >= update_interval:
-            # A.Sensor 
+            # A.Sensor end point to pick up on the dats
             sensor.measure()
             temp = sensor.temperature()
             
-            # B.Hysteresis Logic (Automated Mode)
+            # B.Hysteresis logic (Automated Mode)
             if temp >= TEMP_ON:
                 auto_fan = 1
             elif temp <= TEMP_OFF:
                 auto_fan = 0
             # If the data from the sensor is between 28-30, auto_fan stays at its previous state not snap into an OFF state even at readings like 29.9 leaving the room hot. It cools the room a little more until it gets to 28 and below so that it comes at a halt state
             
-            # C. Priority Logic (The "OR" Logic)
-            # The fan is ON if either the Sensor says so OR the Manual Switch is ON
+            # C. Priority logic (The "OR" Logic)
+            # The fan is ON if either the Sensor says so OR the Manual Overide Switch is ON
             fan_state = 1 if (auto_fan == 1 or manual_fan == 1) else 0
             
-            # D.Apply to Hardware; being a
+            # D. Final fan state value
             fan.value(fan_state)
             
-            # E. Update Blynk Dashboard
+            # E. Send data to the cloud (blynk)
             blynk.virtual_write(1, temp)      # Temperature Gauge DAta
             blynk.virtual_write(2, fan_state) # Fan Status LED Data
             
-            print(f"Temp: {temp}C | Manual: {manual_fan} | Fan: {fan_state}")
+            print(f"Group 5 Fan Info = Temp: {temp}C | Manual Overide Switch: {manual_fan} | Fan: {fan_state}")
             last_update = current_time
 
     except OSError as e:
